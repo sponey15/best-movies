f7340c5a0ad7f9950352dc6841b9e9c36e1c8cc6
sponey15 <gordon4d@gmail.com>
2022-01-27 18:54:48 +0100

    Changed home page month selection

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,6 +17,22 @@ export class HomeComponent implements OnInit {
     'Matrix Zmartwychwstania Dubbing',
     'Matrix Zmartwychwstania Napisy'
   ];
+  months: any[] = [
+    {id:0,name:'Styczeń'},
+    {id:1,name:'Luty'},
+    {id:2,name:'Marzec'},
+    {id:3,name:'Kwiecień'},
+    {id:4,name:'Maj'},
+    {id:5,name:'Czerwiec'},
+    {id:6,name:'Lipiec'},
+    {id:7,name:'Sierpień'},
+    {id:8,name:'Wrzesień'},
+    {id:9,name:'Październik'},
+    {id:10,name:'Listopad'},
+    {id:11,name:'Grudzień'}
+  ];
+  currentMonth: any;
+  currentMonthId: any;
 
   constructor(public cinemaService: CinemaService, private router: Router) { 
     if (sessionStorage.getItem('user')) {
@@ -24,6 +40,8 @@ export class HomeComponent implements OnInit {
     } else {
       this.loggedInUser = false;
     }
+    this.currentMonth = 'Styczeń';
+    this.currentMonthId = 0;
   }
 
   ngOnInit(): void {
@@ -35,5 +53,27 @@ export class HomeComponent implements OnInit {
 
   search() {
     this.router.navigate(['/movie/' + this.selected]);
+  }
+
+  nextMonth() {
+    if (this.currentMonthId == 11) {
+      this.currentMonthId = 0;
+      this.currentMonth = this.months[this.currentMonthId].name;
+    }
+    else {
+      this.currentMonthId += 1;
+      this.currentMonth = this.months[this.currentMonthId].name;
+    }
+  }
+
+  prevMonth() {
+    if (this.currentMonthId == 0) {
+      this.currentMonthId = 0;
+      this.currentMonth = this.months[this.currentMonthId].name;
+    }
+    else {
+      this.currentMonthId -= 1;
+      this.currentMonth = this.months[this.currentMonthId].name;
+    }
   }
 }
